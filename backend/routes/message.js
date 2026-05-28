59d71ed59b034eb1398018ff737a164165b9bb8c
KavyashriGithub <144415609+KavyashriGithub@users.noreply.github.com>
2026-05-28 22:46:26 +0530

    Delete backend directory

--- backend/routes/message.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const messageController = require("../controllers/messageController");
-const userMiddleware = require("../middleware/user");
-
-router.get("/messages/:receiverId", userMiddleware, messageController.getChatMessages);
-
-module.exports = router;
--- backend/routes/userCharts.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const userChartsController = require("../controllers/userChartsController");
-const userMiddleware = require("../middleware/user");
-
-router.get("/user/all", userMiddleware, userChartsController.getAllUser);
-
-module.exports = router;
